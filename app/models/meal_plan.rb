634440a58bc054557226571574db5e96ff4f4601
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-05-02 16:59:42 +0900

    配信の内容を選択する画面を作成中

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,6 +1,6 @@
 class MealPlan < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
-  validates :description, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 200 }
 
   has_many :meal_plan_subscribes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+
+  namespace :users do
+    resources :meal_plan_subscribes, only: %i[create edit update]
+  end
   root 'home#top'
 end
