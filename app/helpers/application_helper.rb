c1dc0fc90fcc3ed680e56b076e02c41ec11c5026
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-05-02 14:29:13 +0900

    ユーザはアカウントの登録ができる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title_dom(title)
+    content_tag :h1, title, class: 'my-4 fs-3'
+  end
+
+  def back_link_dom(text, href)
+    content_tag :a, t('general.to_back', text:), href:, class: 'btn btn-outline-dark'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable, authentication_keys: [:id]
 
+  validates :name, presence: true, length: { maximum: 20 }
+
   # 下記はユーザのメールアドレスを使用しないため、無効化
   def email_required?
     false
